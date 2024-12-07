994fdd755395ed910a8863b9d2ae0977737ee604
Kamran Ayub <kamran@hey.com>
2024-12-06 23:05:32 -0600

    refactor: pattern owned by fns

--- 03/main.ts
+++ 03/main.ts
@@ -1,88 +1,13 @@
 import { sortBy, sumOf } from "@std/collections";
 import { getPuzzleInput } from "./input.ts";
-
-interface Instruction {
-  readonly func: string;
-  readonly text: string;
-  readonly pos: number;
-  exec(instructor: Instructor): void;
-}
-
-interface Calculation extends Instruction {
-  calculate(): number;
-}
-
-class Multiplication implements Calculation {
-  public static Pattern = /mul\((\d+),(\d+)\)/mg;
-  public func = "mul";
-
-  public static create(match: RegExpMatchArray) {
-    if (match.length !== 3) throw new Error("Not a valid Multiplication match");
-
-    const lhs = Number.parseInt(match[1]);
-    const rhs = Number.parseInt(match[2]);
-
-    return new Multiplication(match[0], match.index!, lhs, rhs);
-  }
-
-  private constructor(
-    public text: string,
-    public pos: number,
-    private lhs: number,
-    private rhs: number,
-  ) {}
-
-  public calculate(): number {
-    return this.lhs * this.rhs;
-  }
-
-  public exec(instructor: Instructor): void {
-    if (instructor.enabled) {
-      instructor.calculations.push(this);
-    }
-  }
-}
-
-class Do implements Instruction {
-  public static Pattern = /do\(\)/mg;
-  public func = "do";
-
-  constructor(
-    public text: string,
-    public pos: number,
-  ) {}
-
-  public static create(match: RegExpMatchArray) {
-    return new Do(match[0], match.index!);
-  }
-
-  public exec(instructor: Instructor): void {
-    instructor.enabled = true;
-  }
-}
-
-class Dont implements Instruction {
-  public static Pattern = /don't\(\)/mg;
-  public func = "don't";
-
-  constructor(
-    public text: string,
-    public pos: number,
-  ) {}
-
-  public static create(match: RegExpMatchArray) {
-    return new Dont(match[0], match.index!);
-  }
-
-  public exec(instructor: Instructor): void {
-    instructor.enabled = false;
-  }
-}
-
-class Instructor {
-  public enabled: boolean = true;
-  public calculations: Calculation[] = [];
-}
+import {
+  Calculation,
+  Do,
+  Dont,
+  Instruction,
+  Multiplication,
+} from "./instructions.ts";
+import { Instructor } from "./instructor.ts";
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
@@ -108,7 +33,8 @@ export function sumEnabledInstructionsInMemory(memory: string): number {
 export function findMultiplicationInstructionsInMemory(
   memory: string,
 ): Calculation[] {
-  const matches = [...memory.matchAll(Multiplication.Pattern)];
+  const pattern = /mul\((\d+),(\d+)\)/mg;
+  const matches = [...memory.matchAll(pattern)];
 
   if (matches.length === 0) return [];
 
@@ -139,7 +65,7 @@ function findInstructionsInMemory(memory: string) {
 }
 
 export function findDoInstructionsInMemory(memory: string): Instruction[] {
-  const matches = [...memory.matchAll(Do.Pattern)];
+  const matches = [...memory.matchAll(/do\(\)/mg)];
 
   if (matches.length === 0) return [];
 
@@ -147,7 +73,7 @@ export function findDoInstructionsInMemory(memory: string): Instruction[] {
 }
 
 export function findDontInstructionsInMemory(memory: string): Instruction[] {
-  const matches = [...memory.matchAll(Dont.Pattern)];
+  const matches = [...memory.matchAll(/don't\(\)/mg)];
 
   if (matches.length === 0) return [];
 
