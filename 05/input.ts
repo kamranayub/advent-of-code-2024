ece423752cc19884ce13d32f60466650c46bc6a4
Kamran Ayub <kamran@hey.com>
2024-12-08 22:52:48 -0600

    parse input

--- 05/input.ts
+++ 05/input.ts
@@ -1,8 +1,82 @@
-
 import { getPuzzleInputFromFileUrl } from "@puzzle/shared";
+import { EOL } from "@std/fs";
+
+export const sampleInput = `
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+`;
 
 export function getPuzzleInput() {
   return getPuzzleInputFromFileUrl(
     import.meta.resolve("./input.txt"),
   );
+}
+
+export interface PageRule {
+  pageBefore: number;
+  pageAfter: number;
+}
+
+export type PageUpdate = number[];
+
+export function parsePageRules(input: string): PageRule[] {
+  const lines = input.trim().split(EOL);
+  const rules: PageRule[] = [];
+
+  for (const line of lines) {
+    if (line === "") break;
+
+    const [before, after] = line.split("|");
+
+    rules.push({
+      pageBefore: Number.parseInt(before, 10),
+      pageAfter: Number.parseInt(after, 10),
+    });
+  }
+
+  return rules;
+}
+
+export function parsePageUpdates(input: string): PageUpdate[] {
+  const lines = input.trim().split(EOL);
+  const updates: PageUpdate[] = [];
+
+  const updateStartIndex = lines.findIndex((line) => line === "");
+
+  for (let i = updateStartIndex + 1; i < lines.length; i++) {
+    if (lines[i] === "") continue;
+
+    const pagesRaw = lines[i].split(",");
+    const pages = pagesRaw.map((page) => Number.parseInt(page, 10));
+
+    updates.push(pages);
+  }
+
+  return updates;
 }
--- 05/main.test.ts
+++ 05/main.test.ts
@@ -1,8 +1,27 @@
-
 import { expect } from "@std/expect";
-import { getPuzzleInput } from "./input.ts";
+import {
+  getPuzzleInput,
+  parsePageRules,
+  parsePageUpdates,
+  sampleInput,
+} from "./input.ts";
 
 Deno.test("can read puzzle input", async () => {
   const input = await getPuzzleInput();
   expect(input).toBeDefined();
-})
+});
+
+Deno.test("can parse page rules from input string", () => {
+  const pageRules = parsePageRules(sampleInput);
+
+  expect(pageRules.length).toBe(21);
+  expect(pageRules[0].pageBefore).toBe(47);
+  expect(pageRules[0].pageAfter).toBe(53);
+});
+
+Deno.test("can parse page updates from input string", () => {
+  const pageUpdates = parsePageUpdates(sampleInput);
+
+  expect(pageUpdates.length).toBe(6);
+  expect(pageUpdates[0]).toEqual([75, 47, 61, 53, 29]);
+});
