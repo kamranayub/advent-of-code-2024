99990748205e62535fc1a6735df4b75b669c84a2
Kamran Ayub <kamran@hey.com>
2024-12-08 23:51:57 -0600

    part one done

--- 05/main.test.ts
+++ 05/main.test.ts
@@ -6,13 +6,36 @@ import {
   sampleInput,
 } from "./input.ts";
 import {
+  findMiddlePageInUpdate,
+  findPageRulesAfter,
+  findPageRulesBefore,
   findPageRulesThatApply,
-  findPageRulesThatApplyBefore,
+  sumMiddlePagesInVerifiedUpdates,
+  verifyUpdate,
+  verifyUpdates,
 } from "./main.ts";
+
+Deno.test("can answer part one correctly", async () => {
+  const input = await getPuzzleInput();
+  const pageRules = parsePageRules(input);
+  const pageUpdates = parsePageUpdates(input);
+  const middleSum = sumMiddlePagesInVerifiedUpdates(pageRules, pageUpdates);
+
+  expect(middleSum).toBe(0);
+});
 
 Deno.test("can read puzzle input", async () => {
   const input = await getPuzzleInput();
   expect(input).toBeDefined();
+});
+
+Deno.test("can parse page rules and updates from puzzle input", async () => {
+  const input = await getPuzzleInput();
+  const pageRules = parsePageRules(input);
+  const pageUpdates = parsePageUpdates(input);
+
+  expect(pageRules.length).toBeGreaterThan(100);
+  expect(pageUpdates.length).toBeGreaterThan(100);
 });
 
 Deno.test("can parse page rules from input string", () => {
@@ -43,7 +66,7 @@ Deno.test("can find rules that apply to update", () => {
 
 Deno.test("can find pages in update that must be after a given page", () => {
   const pageRules = parsePageRules(sampleInput);
-  const appliedRules = findPageRulesThatApplyBefore(
+  const appliedRules = findPageRulesAfter(
     pageRules,
     [75, 47, 61, 53, 29],
     75,
@@ -54,4 +77,79 @@ Deno.test("can find pages in update that must be after a given page", () => {
   expect(appliedRules[1]).toEqual({ page: 75, before: 53 });
   expect(appliedRules[2]).toEqual({ page: 75, before: 47 });
   expect(appliedRules[3]).toEqual({ page: 75, before: 61 });
+});
+
+Deno.test("can find pages in update that must be before a given page", () => {
+  const pageRules = parsePageRules(sampleInput);
+  const appliedRules = findPageRulesBefore(
+    pageRules,
+    [75, 47, 61, 53, 29],
+    47,
+  );
+
+  expect(appliedRules.length).toBe(1);
+  expect(appliedRules[0]).toEqual({ page: 75, before: 47 });
+});
+
+Deno.test("can verify update is in the correct order", () => {
+  const pageRules = parsePageRules(sampleInput);
+  const pageUpdates = parsePageUpdates(sampleInput);
+
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[0],
+  )).toBe(true);
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[1],
+  )).toBe(true);
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[2],
+  )).toBe(true);
+});
+
+Deno.test("can verify update is not in order if breaks a rule", () => {
+  const pageRules = parsePageRules(sampleInput);
+  const pageUpdates = parsePageUpdates(sampleInput);
+
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[3],
+  )).toBe(false);
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[4],
+  )).toBe(false);
+  expect(verifyUpdate(
+    pageRules,
+    pageUpdates[5],
+  )).toBe(false);
+});
+
+Deno.test("can list verified updates", () => {
+  const pageRules = parsePageRules(sampleInput);
+  const pageUpdates = parsePageUpdates(sampleInput);
+  const verified = verifyUpdates(pageRules, pageUpdates);
+
+  expect(verified.length).toBe(3);
+});
+
+Deno.test("can find middle page number in verified update", () => {
+  const pageUpdates = parsePageUpdates(sampleInput);
+
+  expect(findMiddlePageInUpdate(pageUpdates[0])).toBe(61);
+  expect(findMiddlePageInUpdate(
+    pageUpdates[1],
+  )).toBe(53);
+  expect(findMiddlePageInUpdate(
+    pageUpdates[2],
+  )).toBe(29);
+});
+
+Deno.test("can sum middle page numbers in verified updates", () => {
+  const pageRules = parsePageRules(sampleInput);
+  const pageUpdates = parsePageUpdates(sampleInput);
+
+  expect(sumMiddlePagesInVerifiedUpdates(pageRules, pageUpdates)).toBe(143);
 });
--- 05/main.ts
+++ 05/main.ts
@@ -1,16 +1,62 @@
+import { sumOf } from "@std/collections";
 import { getPuzzleInput, PageRule, PageUpdate } from "./input.ts";
 
 if (import.meta.main) {
   const puzzleInput = await getPuzzleInput();
 }
 
-export function findPageRulesThatApplyBefore(
+export function sumMiddlePagesInVerifiedUpdates(
+  pageRules: PageRule[],
+  pageUpdates: PageUpdate[],
+) {
+  const verifiedUpdates = verifyUpdates(pageRules, pageUpdates);
+  return sumOf(verifiedUpdates, (update) => findMiddlePageInUpdate(update));
+}
+
+export function findMiddlePageInUpdate(update: PageUpdate) {
+  const middle = (update.length + 1) / 2;
+  return update[middle - 1];
+}
+
+export function verifyUpdates(
+  pageRules: PageRule[],
+  pageUpdates: PageUpdate[],
+) {
+  return pageUpdates.filter((update) => verifyUpdate(pageRules, update));
+}
+
+export function verifyUpdate(pageRules: PageRule[], pageUpdate: PageUpdate) {
+  const appliedRules = findPageRulesThatApply(pageRules, pageUpdate);
+
+  return pageUpdate.every((page, pageIndex) => {
+    const pagesAfter = findPageRulesAfter(appliedRules, pageUpdate, page).map(
+      (rule) => rule.before,
+    );
+    const restOfUpdate = pageUpdate.slice(pageIndex + 1);
+    const restOfUpdateIsAfter = restOfUpdate.every(
+      (pageAfter) => pagesAfter.includes(pageAfter),
+    );
+
+    return restOfUpdateIsAfter;
+  });
+}
+
+export function findPageRulesAfter(
   pageRules: PageRule[],
   pageUpdate: PageUpdate,
   page: number,
 ) {
   const appliedRules = findPageRulesThatApply(pageRules, pageUpdate);
   return appliedRules.filter((rule) => rule.page === page);
+}
+
+export function findPageRulesBefore(
+  pageRules: PageRule[],
+  pageUpdate: PageUpdate,
+  page: number,
+) {
+  const appliedRules = findPageRulesThatApply(pageRules, pageUpdate);
+  return appliedRules.filter((rule) => rule.before === page);
 }
 
 export function findPageRulesThatApply(
